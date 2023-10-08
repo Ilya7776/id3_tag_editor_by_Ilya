e30d7ab83819f3f10940558da0e965d60da85930
Chetyrin_ILya <103918307+Ilya7776@users.noreply.github.com>
2023-10-08 12:37:07 +0300

    tag selection function
    added the function of selecting changeable tags

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import sys
-import music_tag
 from os import listdir
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import sys, music_tag, mutagen
+
 
 
 class Main(QMainWindow):
@@ -19,7 +19,40 @@ class Main(QMainWindow):
         self.folder_link.clicked.connect(self.get_folder)
         self.album_image_link.clicked.connect(self.get_image_for_album)
         self.clear_button.clicked.connect(self.clear)
+        self.check_artist.clicked.connect(self.checkbox_return)
+        self.check_album.clicked.connect(self.checkbox_return)
+        self.check_genre.clicked.connect(self.checkbox_return)
+        self.check_image.clicked.connect(self.checkbox_return)
     
+    def checkbox_return(self):
+        if self.check_artist.isChecked():
+            self.label_artist.setEnabled(True)
+            self.input_artist.setEnabled(True)
+        else:
+            self.label_artist.setDisabled(True)
+            self.input_artist.setDisabled(True)
+        
+        if self.check_album.isChecked():
+            self.label_album.setEnabled(True)
+            self.input_album.setEnabled(True)
+        else:
+            self.label_album.setDisabled(True)
+            self.input_album.setDisabled(True)
+        
+        if self.check_genre.isChecked():
+            self.label_genre.setEnabled(True)
+            self.input_genre.setEnabled(True)
+        else:
+            self.label_genre.setDisabled(True)
+            self.input_genre.setDisabled(True)
+        
+        if self.check_image.isChecked():
+            self.image_of_album.setEnabled(True)
+            self.album_image_link.setEnabled(True)
+        else:
+            self.image_of_album.setDisabled(True)
+            self.album_image_link.setDisabled(True)
+            
     def get_folder(self):
         self.folder = QFileDialog.getExistingDirectory(self,"Select folder",".")
         self.messege.setText("<br>you have selected a folder: <b>{}</b>".format(self.folder))
@@ -42,19 +75,29 @@ class Main(QMainWindow):
         f = music_tag.load_file(self.folder + "/" + track)
         print(f)
         
-        if self.album_image != '':
-            with open(self.album_image, 'rb') as img_in:
-                f['artwork'] = img_in.read()
-                
-        f.remove_tag('album')
-        f.remove_tag('artist')
-        f.remove_tag('genre')
+        if self.check_artist.isChecked():
+            f.remove_tag('artist')
+            f['artist'] = self.artist
+
+        
+        if self.check_album.isChecked():
+            f.remove_tag('album')
+            f['album'] = self.album
+
+        
+        if self.check_genre.isChecked():
+            f.remove_tag('genre')
+            f['genre'] = self.genre
+
+        
+        if self.check_image.isChecked():
+            if self.album_image != '':
+                with open(self.album_image[0], 'rb') as img_in:
+                    f['artwork'] = img_in.read()
+        
         f.remove_tag('comment')
-        f['album'] = self.album
-        f['artist'] = self.artist
-        f['genre'] = self.genre
-        f['comment'] = self.comment
-                
+        f['comment'] = "redactor_ID3fromIly"
+
         f.save()
     
     def master_traks(self):
